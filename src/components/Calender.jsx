125c117f301f79f95efe94616aad5bf71a2a4d54
INSAEN <pratham1986@gmail.com>
2023-03-06 03:42:15 +0530

    eventmodal created

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -3,28 +3,33 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import events from "./events";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { modalState } from "../atoms/modalState";
+import { useRecoilState } from "recoil";
+import EventModal from "./EventModal";
 moment.locale("en-GB");
 const localizer = momentLocalizer(moment);
 
 export default function Calender() {
   const [eventsData, setEventsData] = useState(events);
+  const [modal, setModal] = useRecoilState(modalState);
   const handleClick = (event) => {
     alert(event.title);
   };
   const handleSelect = ({ start, end }) => {
-    const title = window.prompt("New Event name");
-    if (title)
-      setEventsData([
-        ...eventsData,
-        {
-          title,
-          start,
-          end,
-        },
-      ]);
+    // const title = window.prompt("New Event name");
+    setModal(true);
+    // if (title)
+    //   setEventsData([
+    //     ...eventsData,
+    //     {
+    //       title,
+    //       start,
+    //       end,
+    //     },
+    //   ]);
   };
   return (
-    <div className="flex flex-col ">
+    <div className="flex flex-col">
       <div className="flex items-center justify-center mb-4">
         <button
           className="bg-[#AA5656] text-[#F1DBBF] font-[Poppins] py-2 px-8 mx-2 rounded  hover:bg-[#F1DBBF] hover:text-[#AA5656] duration-500"
@@ -33,6 +38,7 @@ export default function Calender() {
           Add Lecture
         </button>
       </div>
+      {modal ? <EventModal /> : null}
       <Calendar
         views={["day", "week"]}
         selectable
