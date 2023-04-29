b76d23e99172f395943468d6f819392475b9715d
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-04-30 02:23:25 +0530

    helleleeo

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,75 +1,71 @@
-import { Select } from 'antd';
-import { useState } from 'react';
-// const onChange = (value) => {
-//   console.log(`selected ${value}`);
-// };
-// const onSearch = (value) => {
-//   console.log('search:', value);
-// };
+import { Select } from "antd";
+import { useState } from "react";
+
 const App = () => {
-    const[clicked, setClicked] = useState(false)
-    const[clicked2, setClicked2] = useState(false)
-    const onChange = (value) => {
-        console.log(`selected ${value}`);
-        setClicked(true)
-    }
-    const onChange2 = (value) => {
-        console.log(`selected ${value}`);
-        setClicked2(true)
-    }
-    return (
-    <div>
-  <Select
-    style={{ width: 250, marginLeft: 65 }}
-    showSearch
-    placeholder="Select year"
-    optionFilterProp="children"
-    onChange={onChange}
-    // onSearch={onSearch}
-    filterOption={(input, option) =>
-      (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
-    }
-    options={[
-      {
-        value: 'firstYear',
-        label: 'first year',
-      },
-      {
-        value: 'secondYear',
-        label: 'second year',
-      },
-      {
-        value: 'thirdYear',
-        label: 'third year',
-      },
-      {
-        value: 'fourthYear',
-        label: 'fourth year',
-      }
-    ]}
-  />
-  {clicked?<Select
-    style={{ width: 250, marginLeft: 65 }}
-    showSearch
-    placeholder="Select division"
-    optionFilterProp="children"
-    onChange={onChange2}
-    // onSearch={onSearch}
-    filterOption={(input, option) =>
-      (option?.label ?? '').toString().includes(input.toString())
-    }
-    options={[
-      {
-        value: 'divisionA',
-        label: 'division A',
-      },
-      {
-        value: 'divisionB',
-        label: 'division B',
-      }
-    ]}
-  />:<div></div>}
-  {clicked2?<Select
+  const [year, setYear] = useState("");
+  const [division, setDivision] = useState("");
+
+  const onChange = (value) => {
+    setYear(value);
+  };
+
+  const onChange1 = (value) => {
+    setDivision(value);
+  };
+  return (
+    <div className="flex gap-8">
+      <Select
+        style={{ width: 250 }}
+        showSearch
+        id="year"
+        placeholder="Select year"
+        optionFilterProp="children"
+        onChange={onChange}
+        defaultValue={year}
+        filterOption={(input, option) =>
+          (option?.label ?? "").toLowerCase().includes(input.toLowerCase())
+        }
+        options={[
+          {
+            value: "firstYear",
+            label: "first year",
+          },
+          {
+            value: "secondYear",
+            label: "second year",
+          },
+          {
+            value: "thirdYear",
+            label: "third year",
+          },
+          {
+            value: "fourthYear",
+            label: "fourth year",
+          },
+        ]}
+      />
+      <Select
+        style={{ width: 250 }}
+        showSearch
+        id="division"
+        placeholder="Select division"
+        optionFilterProp="children"
+        onChange={onChange1}
+        filterOption={(input, option) =>
+          (option?.label ?? "").toString().includes(input.toString())
+        }
+        options={[
+          {
+            value: "divisionA",
+            label: "division A",
+          },
+          {
+            value: "divisionB",
+            label: "division B",
+          },
+        ]}
+      />
+      {/* <Select
     style={{ width: 250, marginLeft: 65 }}
     showSearch
     placeholder="Select batch"
@@ -89,8 +85,8 @@ const App = () => {
         label: 'batch 2',
       }
     ]}
-  />:<div></div>}
-  </div>
-    );
+  /> */}
+    </div>
+  );
 };
 export default App;
--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import Nav from '../components/Navbar'
-import Banner from '../components/Banner'
-
+import React from "react";
+import Nav from "../components/Navbar";
+import Banner from "../components/Banner";
+import Filters from "../components/Filters";
 const ReportPage = () => {
   return (
-    <div><Nav />
-    <Banner />ReportPage</div>
-  )
-}
+    <div>
+      <Nav />
+      {/* <Banner /> */}
+      <div className="flex flex-col gap-y-4 justify-center items-center">
+        <h1 className="text-4xl font-bold text-black">Report Page</h1>
+        <Filters />
+      </div>
+    </div>
+  );
+};
 
-export default ReportPage
+export default ReportPage;
 
 //getdefaulterslistbyclass -- columns = subjects row= name, attendance per subject () (high priority)
 //showstudentdetails -- columns = subjects row= name, attendance per subject but for only one student, in detail (low p[riority])
