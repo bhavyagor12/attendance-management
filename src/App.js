540fe4447aa32a7c0eef3a0c3736254d50d0a258
INSAEN <pratham1986@gmail.com>
2023-07-29 22:54:59 +0530

    day to date func // temp

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ import SubjectPage from "./pages/SubjectPage";
 import StudentPage from "./pages/StudentPage";
 import ReportPage from "./pages/ReportPage";
 function App() {
+  function getDaysDate(day) {
+    const mapDayToNumber = {
+      sunday: 0,
+      monday: 1,
+      tuesday: 2,
+      wednesday: 3,
+      thursday: 4,
+      friday: 5,
+      saturday: 6
+    }
+    day = mapDayToNumber[day.toLowerCase()];
+    const today = new Date();
+    const currentDayOfWeek = today.getDay(); // Sunday is 0, Monday is 1, and so on.
+    const daysUntilDay = currentDayOfWeek === day-1 ? 6 : day - currentDayOfWeek;
+    // Calculate the date of Monday by subtracting the number of days from today.
+    const dayDate = new Date(today);
+    dayDate.setDate(today.getDate() + daysUntilDay);
+    return dayDate;
+  }
+  const dayDate = getDaysDate("wednesday");
+  console.log(dayDate.toDateString()); // Output: The date of the current week's Monday
   return (
     <div>
       <BrowserRouter>
