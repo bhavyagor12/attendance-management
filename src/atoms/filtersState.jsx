42220f605aa25b844d842ad053dd1fece8b4f270
Bhavya Gor <bhavya.gor9999@gmail.com>
2023-08-24 23:15:12 +0530

    year chnages

--- src/atoms/filtersState.jsx
+++ src/atoms/filtersState.jsx
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 export const filtersState = atom({
   key: "filters",
-  default: { year: 2024, division: "A", startDate: "2023-06-01", endDate: "2023-09-14" },
+  default: {
+    year: 3,
+    division: "A",
+    startDate: "2023-06-01",
+    endDate: "2023-09-14",
+  },
 });
--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -13,7 +13,7 @@ export default function EventModal({ startD, endD }) {
   const [SubjectCode, setSubjectCode] = useState("");
   const [SubjectName, setSubjectName] = useState("");
   const [division, setDivision] = useState("A");
-  const [year, setYear] = useState("FE");
+  const [year, setYear] = useState(3);
   // const [type, setType] = useState("Lecture");
   const [startTime, setStartTime] = useState();
   const [endTime, setEndTime] = useState(startTime);
@@ -56,7 +56,7 @@ export default function EventModal({ startD, endD }) {
       setEventsData([
         ...eventsData,
         {
-          title: SubjectName,//TODO: somehow get the subject name
+          title: SubjectName, //TODO: somehow get the subject name
           start: new Date(startTime),
           end: new Date(endTime),
         },
@@ -79,8 +79,7 @@ export default function EventModal({ startD, endD }) {
         as="div"
         className="relative z-10"
         initialFocus={cancelButtonRef}
-        onClose={setModal}
-      >
+        onClose={setModal}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -88,8 +87,7 @@ export default function EventModal({ startD, endD }) {
           enterTo="opacity-100"
           leave="ease-in duration-200"
           leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
+          leaveTo="opacity-0">
           <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
@@ -102,8 +100,7 @@ export default function EventModal({ startD, endD }) {
               enterTo="opacity-100 translate-y-0 sm:scale-100"
               leave="ease-in duration-200"
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pt-4 pb-4 sm:p-6 sm:pb-4">
                   <div className="flex items-center">
@@ -116,35 +113,35 @@ export default function EventModal({ startD, endD }) {
                     <div className="text-left sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
-                      >
+                        className="text-base font-semibold leading-6 text-gray-900">
                         Create Lecture
                       </Dialog.Title>
                     </div>
                   </div>
                   <div className="text-center mt-4 px-2 flex flex-col">
-                  <div className="flex flex-col items-start my-1">
+                    <div className="flex flex-col items-start my-1">
                       <label>Year</label>
                       <select
-                      onChange={(e) => {
-                        setYear(e.target.value);
-                        setError(false);
-                        if (e.target.value === "") {
-                          setError(true);
-                        }
-                      }}
-                      name="Year"
-                      id="Year"
-                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-offset-0 sm:text-sm sm:leading-6"
-                      // value={year} // Make sure to set the value here if you want to show a default value
-                    >
-                      <option value="">Select Year</option> {/* Empty value for default selection */}
-                      <option value="1">FE</option>
-                      <option value="2">SE</option>
-                      <option value="3">TE</option>
-                      <option value="4">BE</option>
-                      {/* Add more options as needed */}
-                    </select>
+                        onChange={(e) => {
+                          setYear(e.target.value);
+                          setError(false);
+                          if (e.target.value === "") {
+                            setError(true);
+                          }
+                        }}
+                        name="Year"
+                        id="Year"
+                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-offset-0 sm:text-sm sm:leading-6"
+                        // value={year} // Make sure to set the value here if you want to show a default value
+                      >
+                        <option value="">Select Year</option>{" "}
+                        {/* Empty value for default selection */}
+                        <option value="1">FE</option>
+                        <option value="2">SE</option>
+                        <option value="3">TE</option>
+                        <option value="4">BE</option>
+                        {/* Add more options as needed */}
+                      </select>
 
                       {error && (
                         <div className="text-red-400 text-sm">
@@ -198,29 +195,7 @@ export default function EventModal({ startD, endD }) {
                         </div>
                       )}
                     </div>
-                    {/* <div className="flex flex-col items-start my-1">
-                      <label>Subject Name</label>
-                      <input
-                        onChange={(e) => {
-                          setSubjectName(e.target.value);
-                          setError(false);
-                          if (e.target.value === "") {
-                            setError(true);
-                          }
-                        }}
-                        type="text"
-                        name="SubjectName"
-                        id="SubjectName"
-                        className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-0 focus:ring-offset-0 sm:text-sm sm:leading-6"
-                        placeholder="Subject Name"
-                      />
 
-                      {error && (
-                        <div className="text-red-400 text-sm">
-                          please enter a valid subject name
-                        </div>
-                      )}
-                    </div> */}
                     <div className="flex flex-col items-start my-1">
                       <label>Start Time</label>
                       <input
@@ -251,16 +226,14 @@ export default function EventModal({ startD, endD }) {
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto"
-                    onClick={onSubmit}
-                  >
+                    onClick={onSubmit}>
                     Create
                   </button>
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50 sm:mt-0 sm:w-auto"
                     onClick={() => setModal(false)}
-                    ref={cancelButtonRef}
-                  >
+                    ref={cancelButtonRef}>
                     Cancel
                   </button>
                 </div>
